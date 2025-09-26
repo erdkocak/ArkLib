2d76ae1c4d14356c7abcf2b456b1efc5523065cd
Quang Dao <qvd@andrew.cmu.edu>
2025-09-26 10:29:15 -0400

    more update

--- Examples/Sumcheck.lean
+++ Examples/Sumcheck.lean
@@ -8,7 +8,9 @@ set_option linter.style.longLine false
 
 universe u v w
 
-open Polynomial Bivariate
+open Polynomial Bivariate NNReal
+
+local instance {F : Type*} [Field F] : Nonempty F := ⟨0⟩
 
 namespace Example
 
@@ -32,6 +34,22 @@ def single (Q : Type u) (R : Type v) : OracleSpec :=
 
 instance : Add OracleSpec where
   add := add
+
+protected class Fintype (spec : OracleSpec) where
+  [instQuery : Fintype spec.Query]
+  [instResponse : ∀ q, Fintype (spec.Response q)]
+
+instance {spec : OracleSpec} [spec.Fintype] : Fintype spec.Query := Fintype.instQuery
+instance {spec : OracleSpec} [spec.Fintype] (q : spec.Query) : Fintype (spec.Response q) :=
+  Fintype.instResponse q
+
+protected class Nonempty (spec : OracleSpec) where
+  [instQuery : Nonempty spec.Query]
+  [instResponse : ∀ q, Nonempty (spec.Response q)]
+
+instance {spec : OracleSpec} [spec.Nonempty] : Nonempty spec.Query := Nonempty.instQuery
+instance {spec : OracleSpec} [spec.Nonempty] (q : spec.Query) : Nonempty (spec.Response q) :=
+  Nonempty.instResponse q
 
 end OracleSpec
 
@@ -58,11 +76,11 @@ instance : Monad (OracleComp spec) where
   bind := OracleComp.bind
 
 @[simp]
-lemma monad_pure_def (x : α) : (pure x : OracleComp spec α) = OracleComp.pure x := rfl
+lemma pure_eq_monad_pure (x : α) : OracleComp.pure x = (pure x : OracleComp spec α) := rfl
 
 @[simp]
-lemma monad_bind_def (x : OracleComp spec α) (f : α → OracleComp spec β) :
-    x >>= f = OracleComp.bind x f := rfl
+lemma bind_eq_monad_bind (oa : OracleComp spec α) (f : α → OracleComp spec β) :
+    OracleComp.bind oa f = (oa >>= f : OracleComp spec β) := rfl
 
 instance : LawfulMonad (OracleComp spec) :=
   LawfulMonad.mk' (OracleComp spec)
@@ -95,6 +113,48 @@ instance : MonadLift (OracleComp spec) (OracleComp (spec + spec')) where
 
 instance : MonadLift (OracleComp spec') (OracleComp (spec + spec')) where
   monadLift := inr
+
+section Support
+
+def support (oa : OracleComp spec α) : Set α :=
+  match oa with
+  | pure a => {a}
+  | queryBind q cont => ⋃ x : spec.Response q, support (cont x)
+
+@[simp]
+lemma support_pure (a : α) : support (pure a : OracleComp spec α) = {a} := rfl
+
+@[simp]
+lemma support_queryBind (q : spec.Query) (cont : spec.Response q → OracleComp spec α) :
+    support (queryBind q cont) = ⋃ x : spec.Response q, support (cont x) := rfl
+
+@[simp]
+lemma support_bind (oa : OracleComp spec α) (f : α → OracleComp spec β) :
+    support (oa >>= f) = ⋃ x : oa.support, support (f x) := by
+  induction oa with
+  | pure a => simp; rfl
+  | queryBind q cont ih => simp [support_queryBind]; sorry
+
+end Support
+
+section EvalDist
+
+variable [spec.Fintype] [spec.Nonempty]
+
+-- Distributional semantics via responding uniformly at random
+noncomputable def evalDist (oa : OracleComp spec α) : PMF α :=
+  match oa with
+  | pure a => PMF.pure a
+  | queryBind q cont => do
+      let r ← PMF.uniformOfFintype (spec.Response q)
+      evalDist (cont r)
+
+noncomputable instance instHasEvalDist : HasEvalDist (OptionT (OracleComp spec)) where
+  evalDist := fun oa => evalDist (oa.run)
+  evalDist_pure := fun x => by simp [evalDist]; rfl
+  evalDist_bind := fun oa cont => sorry
+
+end EvalDist
 
 end OracleComp
 
@@ -157,7 +217,7 @@ variable {m₁ m₂ : Type → Type} [Monad m₁] [Monad m₂] [MonadLiftT m₁ 
 
 /-- Change the monad of an interactive protocol by lifting its internal effects. -/
 @[simp]
-def mapLift (pSpec : ProtocolSpec)
+def liftM (pSpec : ProtocolSpec)
     {Output : Type} :
     InteractiveProtocol m₁ pSpec Output → InteractiveProtocol m₂ pSpec Output :=
   match pSpec with
@@ -167,11 +227,11 @@ def mapLift (pSpec : ProtocolSpec)
       | (msg, cont) =>
         (msg, monadLift (do
           let rest ← cont
-          pure (mapLift ps rest)))
+          pure (liftM ps rest)))
   | (.Receive, _) :: ps => fun ip =>
       (fun t => monadLift (do
         let rest ← ip t
-        pure (mapLift ps rest)))
+        pure (liftM ps rest)))
 
 /-- Execute an interactive protocol by coordinating message exchanges between two parties.
     Returns the complete transcript along with both parties' outputs. -/
@@ -221,11 +281,10 @@ structure Reduction (mP mV : Type → Type)
 namespace Reduction
 
 variable {mP mV : Type → Type} {m : Type → Type}
-  [Monad mP] [Monad mV] [Monad m] [MonadLiftT mP m] [MonadLiftT mV m]
+  [Monad mP] [Monad mV] [Monad m] [MonadLiftT mP m] [MonadLiftT mV m] [HasEvalDist m]
   {InputStatement InputWitness : Type}
   {OutputStatement OutputWitness : Type}
   {pSpec : ProtocolSpec}
-
 
 /-- Execute a reduction by running the interactive protocol between prover and verifier.
     Takes an input statement and witness, applies the reduction's prover and verifier strategies,
@@ -239,13 +298,52 @@ def run (inputStatement : InputStatement) (inputWitness : InputWitness)
   let initialVerifierProtocol := reduction.verifier inputStatement
   -- Lift both protocols to the common monad m
   let liftedProverProtocol : InteractiveProtocol m pSpec (OutputStatement × OutputWitness) :=
-    InteractiveProtocol.mapLift pSpec initialProverProtocol
+    InteractiveProtocol.liftM pSpec initialProverProtocol
   let liftedVerifierProtocol : InteractiveProtocol m pSpec.flip OutputStatement :=
-    InteractiveProtocol.mapLift pSpec.flip initialVerifierProtocol
+    InteractiveProtocol.liftM pSpec.flip initialVerifierProtocol
   -- Execute the interactive protocol between the two parties
   InteractiveProtocol.run liftedProverProtocol liftedVerifierProtocol
 
+/-- Perfect completeness of a reduction: for every input statement and witness, if the input
+relation holds, then the output relation holds with probability 1. -/
+def completeness
+    (inputRelation : Set (InputStatement × InputWitness))
+    (outputRelation : Set (OutputStatement × OutputWitness))
+    (reduction : Reduction mP mV InputStatement InputWitness OutputStatement OutputWitness pSpec) :
+      Prop :=
+  ∀ inputStatement : InputStatement,
+  ∀ inputWitness : InputWitness,
+    (inputStatement, inputWitness) ∈ inputRelation →
+      Pr[ fun ⟨_, ⟨prvOutputStatement, outputWitness⟩, verOutputStatement⟩
+          => (verOutputStatement, outputWitness) ∈ outputRelation
+              ∧ prvOutputStatement = verOutputStatement
+          | reduction.run (m := m) inputStatement inputWitness] = 1
+
 end Reduction
+
+namespace Verifier
+
+variable {mV : Type → Type} [Monad mV]
+  {InputStatement OutputStatement : Type} {pSpec : ProtocolSpec}
+
+/-- Soundness of a reduction: given an input statement not in the input language,
+  any **arbitrary** prover interacting with the verifier will cause the verifier to output an
+  output statement not in the output language,
+  **except** with probability at most `soundnessError`. -/
+def soundness
+    (inputLanguage : Set InputStatement)
+    (outputLanguage : Set OutputStatement)
+    (verifier : Verifier mV InputStatement OutputStatement pSpec)
+    (m : Type → Type) [Monad m] [HasEvalDist m] [MonadLiftT mV m]
+    (soundnessError : ℝ≥0) : Prop :=
+  ∀ inputStatement : InputStatement,
+  -- Prover strategy without inputs or outputs
+  ∀ prover : InteractiveProtocol m pSpec Unit,
+    inputStatement ∉ inputLanguage →
+    Pr[ fun ⟨_, _, outputStatement⟩ => outputStatement ∈ outputLanguage
+      | InteractiveProtocol.run (m := m) prover (verifier inputStatement).liftM] ≤ soundnessError
+
+end Verifier
 
 /-! ## We specify the (two-round) sum-check protocol based on our definitions -/
 
@@ -294,8 +392,17 @@ def pSpec : ProtocolSpec := [(.Send, F[X]), (.Receive, F), (.Send, F[X]), (.Rece
 #reduce (types := true) Transcript (pSpec F)
 
 /-- The field sampling oracle: given a dummy query `()`, returns a random element of `F` -/
+@[reducible]
 def fieldSamplingOracle : OracleSpec :=
   { Query := Unit, Response := fun _ => F }
+
+instance : OracleSpec.Fintype (fieldSamplingOracle F) :=
+  { instQuery := inferInstance
+    instResponse := inferInstance }
+
+instance : OracleSpec.Nonempty (fieldSamplingOracle F) :=
+  { instQuery := inferInstance
+    instResponse := inferInstance }
 
 /-- Sample a random field element -/
 def sampleField : OracleComp (fieldSamplingOracle F) F := OracleComp.query ()
@@ -314,7 +421,7 @@ def InputStatement := F⦃≤ d⦄[X][Y] × F
 def InputWitness := Unit
 
 /-- Input relation: p(0,0) + p(0,1) + p(1,0) + p(1,1) = t -/
-def inputRelation : InputStatement F d × InputWitness → Prop :=
+def inputRelation : Set (InputStatement F d × InputWitness) :=
   fun ⟨⟨⟨p, _⟩, t⟩, _⟩ => p.evalXY 0 0 + p.evalXY 0 1 + p.evalXY 1 0 + p.evalXY 1 1 = t
 
 /-- Output: polynomial p, challenges r₁, r₂, and final claim t₂ -/
@@ -324,11 +431,13 @@ def OutputStatement : Type := F⦃≤ d⦄[X][Y] × F × F × F
 def OutputWitness := Unit
 
 /-- Output relation: p(r₁, r₂) = t₂ -/
-def outputRelation : OutputStatement F d × OutputWitness → Prop :=
+def outputRelation : Set (OutputStatement F d × OutputWitness) :=
   fun ⟨⟨⟨p, _⟩, r₁, r₂, t₂⟩, _⟩ => p.evalXY r₁ r₂ = t₂
 
+noncomputable section
+
 /-- Prover strategy: compute s₁ = p(X,0) + p(X,1), then s₂ = p(r₁,X) -/
-noncomputable def prover : Prover Id
+def prover : Prover Id
     (InputStatement F d) InputWitness (OutputStatement F d) OutputWitness (pSpec F) :=
   fun ⟨⟨⟨p, h⟩, _⟩, _⟩ => Id.run do
     -- Step 1: Send s₁(X) = p(X,0) + p(X,1)
@@ -342,7 +451,7 @@ noncomputable def prover : Prover Id
 
 open Classical in
 /-- Verifier strategy: check degree bounds, consistency, and sample random challenges -/
-noncomputable def verifier : Verifier (OptionT (OracleComp (fieldSamplingOracle F + bivariatePolyOracle F)))
+def verifier : Verifier (OptionT (OracleComp (fieldSamplingOracle F)))
     (InputStatement F d) (OutputStatement F d) (pSpec F) :=
   fun ⟨p, t⟩ s₁ => do
       -- Step 2: Check deg s₁ ≤ d and s₁(0) + s₁(1) = t, then sample r₁
@@ -358,13 +467,27 @@ noncomputable def verifier : Verifier (OptionT (OracleComp (fieldSamplingOracle 
         return (r₂, pure (p, r₁, r₂, t₂)))
 
 /-- The (two-round) sum-check protocol as a reduction -/
-noncomputable def reduction : Reduction
-    Id (OptionT (OracleComp (fieldSamplingOracle F + bivariatePolyOracle F)))
+def reduction : Reduction
+    Id (OptionT (OracleComp (fieldSamplingOracle F)))
     (InputStatement F d) InputWitness (OutputStatement F d) OutputWitness (pSpec F) where
   prover := prover F d
   verifier := verifier F d
 
--- TODO: define and prove completeness and soundness
+theorem reduction_completeness :
+    (reduction F d).completeness (m := OptionT (OracleComp (fieldSamplingOracle F)))
+    (inputRelation F d) (outputRelation F d) := by
+  intro inputStatement inputWitness isValid
+  simp [reduction, Reduction.run]
+  sorry
+
+theorem reduction_soundness :
+    (verifier F d).soundness (m := OptionT (OracleComp (fieldSamplingOracle F)))
+    (inputRelation F d).language (outputRelation F d).language
+    ((2 * d : ℝ≥0) / Fintype.card F) := by
+  intro inputStatement inputWitness isValid
+  sorry
+
+end
 
 end Sumcheck
 
