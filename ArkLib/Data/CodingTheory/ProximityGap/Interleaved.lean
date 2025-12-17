752d3ec1d4401d67d8ae7ffad1b8e4bf2ab699fd
Alexander Hicks <alexanderlhicks@gmail.com>
2025-12-17 23:14:21 +0000

    carry over authors from AHIV22 to Interleaved proximity gaps

--- ArkLib/Data/CodingTheory/ProximityGap/Interleaved.lean
+++ ArkLib/Data/CodingTheory/ProximityGap/Interleaved.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 - 2025 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors : Chung Thai Nguyen, Quang Dao
+Authors : Chung Thai Nguyen, Quang Dao, Katerina Hristova, František Silváši
 -/
 import ArkLib.Data.Nat.Bitwise
 import ArkLib.Data.CodingTheory.Basic
