f61be23487e5dd55aa0bc988da7f7b3c622e6505
Alexander Hicks <alexanderlhicks@gmail.com>
2025-12-18 00:26:46 +0000

    fix import/build error

--- ArkLib.lean
+++ ArkLib.lean
@@ -180,7 +180,7 @@ import ArkLib.ProofSystem.Spartan.Basic
 import ArkLib.ProofSystem.Stir
 import ArkLib.Data.CodingTheory.Operations.Combine
 import ArkLib.Data.CodingTheory.Folding.Stir
-import ArkLib.ProofSystem.Stir.MainThm
+import ArkLib.ProofSystem.Stir.RBRSoundness
 import ArkLib.Data.CodingTheory.OutOfDomSmpl.Stir
 import ArkLib.Data.CodingTheory.Proximity.Bound
 import ArkLib.Data.CodingTheory.Operations.Quotienting
--- ArkLib/ProofSystem/Stir.lean
+++ ArkLib/ProofSystem/Stir.lean
@@ -1,6 +1,6 @@
 import ArkLib.Data.CodingTheory.Operations.Combine
 import ArkLib.Data.CodingTheory.Folding.Stir
-import ArkLib.ProofSystem.Stir.MainThm
+import ArkLib.ProofSystem.Stir.RBRSoundness
 import ArkLib.Data.CodingTheory.OutOfDomSmpl.Stir
 import ArkLib.Data.CodingTheory.Proximity.Bound
 import ArkLib.Data.CodingTheory.Operations.Quotienting
